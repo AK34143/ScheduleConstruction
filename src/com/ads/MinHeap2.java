9083e9465d75244efe391ce74d8f6ac3d150f374
AK341 <aarthi.kashikar2@gmail.com>
2019-11-18 22:40:23 -0500

    MinHeap implementation(names are still max related).Implemented 2D array

--- src/com/ads/MinHeap2.java
+++ src/com/ads/MinHeap2.java
@@ -3,6 +3,8 @@ package com.ads;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 
+import static com.ads.Main2.triple;
+
 public class MinHeap2 {
 
 
@@ -12,7 +14,7 @@ public class MinHeap2 {
      */
 
         private static final int d= 2;
-        private int[] heap;
+        private int[][] heap;
         private int heapSize;
 
         /**
@@ -20,8 +22,8 @@ public class MinHeap2 {
          */
         public MinHeap2(int capacity){
             heapSize = 0;
-            heap = new int[ capacity+1];
-            Arrays.fill(heap, -1);
+            heap = new int[capacity+1][2];
+            Arrays.fill(heap, new int[]{-1,-1});
 
         }
 
@@ -55,7 +57,7 @@ public class MinHeap2 {
          *  Complexity: O(log N)
          *  As worst case scenario, we need to traverse till the root
          */
-        public void insert(int x){
+        public void insert(int[] x){
             if(isFull())
                 throw new NoSuchElementException("Heap is full, No space to insert new element");
             heap[heapSize++] = x;
@@ -70,7 +72,7 @@ public class MinHeap2 {
         public int delete(int x){
             if(isEmpty())
                 throw new NoSuchElementException("Heap is empty, No element to delete");
-            int key = heap[x];
+            int key = heap[x][0];
             heap[x] = heap[heapSize -1];
             heapSize--;
             heapifyDown(x);
@@ -82,8 +84,8 @@ public class MinHeap2 {
          *
          */
         private void heapifyUp(int i) {
-            int temp = heap[i];
-            while(i>0 && temp > heap[parent(i)]){
+            int[] temp = heap[i];
+            while(i>0 && temp[1] < heap[parent(i)][1]){
                 heap[i] = heap[parent(i)];
                 i = parent(i);
             }
@@ -96,10 +98,10 @@ public class MinHeap2 {
          */
         private void heapifyDown(int i){
             int child;
-            int temp = heap[i];
+            int[] temp = heap[i];
             while(kthChild(i, 1) < heapSize){
                 child = maxChild(i);
-                if(temp < heap[child]){
+                if(temp[1] > heap[child][1]){
                     heap[i] = heap[child];
                 }else
                     break;
@@ -113,7 +115,7 @@ public class MinHeap2 {
             int leftChild = kthChild(i, 1);
             int rightChild = kthChild(i, 2);
 
-            return heap[leftChild]>heap[rightChild]?leftChild:rightChild;
+            return heap[leftChild][1]<heap[rightChild][1]?leftChild:rightChild;
         }
 
         /**
@@ -124,35 +126,72 @@ public class MinHeap2 {
         {
             System.out.print("\nHeap = ");
             for (int i = 0; i < heapSize; i++)
-                System.out.print(heap[i] +" ");
+                System.out.print("("+heap[i][0]+","+heap[i][1] +","+heap[i][2]+") ");
             System.out.println();
         }
         /**
          *  This method returns the max element of the heap.
          *  complexity: O(1)
          */
-        public int findMax(){
+        public int[] findMax(){
             if(isEmpty())
                 throw new NoSuchElementException("Heap is empty.");
             return heap[0];
         }
 
+    public static int[] stringToParams(String input) {
+        input = input.trim();
+        //input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+
+        String[] params = input.split(",");
+        int[] output = new int[3];
+      /*  String part = params[0].split("\\(")[1];
+        output[0] = Integer.parseInt(part);
+        output[1]=0;
+        part = params[1].split("\\)")[0];
+        output[2] = Integer.parseInt(part);*/
+        String part = params[0].trim();
+        output[0] = Integer.parseInt(part);
+        part = params[1].trim();
+        output[1]=Integer.parseInt(part);
+        part = params[2].trim();
+        output[2] = Integer.parseInt(part);
+        /*for(int index = 0; index < params.length; index++) {
+            if(index==1){
+                output[index]=0;
+            }else {
+                String part = params[index].trim();
+                output[index] = Integer.parseInt(part);
+            }
+        }*/
+        return output;
+    }
+
         public static void main(String[] args){
             MinHeap2 maxHeap = new MinHeap2(10);
-            maxHeap.insert(1);
+            int[] params = stringToParams("1,5,25");
+            maxHeap.insert(params);
             maxHeap.printHeap();
-            maxHeap.insert(4);
+            params = stringToParams("4,6,10");
+            maxHeap.insert(params);
             maxHeap.printHeap();
-            maxHeap.insert(9);
+            params = stringToParams("9,3,15");
+            maxHeap.insert(params);
             maxHeap.printHeap();
-            maxHeap.insert(10);
+            params = stringToParams("10,7,3");
+            maxHeap.insert(params);
             maxHeap.printHeap();
-            maxHeap.insert(7);
+            params = stringToParams("7,10,4");
+            maxHeap.insert(params);
             maxHeap.printHeap();
-            maxHeap.insert(5);
+            /*maxHeap.insert(5);
             maxHeap.printHeap();
             maxHeap.insert(3);
-            maxHeap.printHeap();
+            maxHeap.printHeap();*/
             maxHeap.delete(0);
             maxHeap.printHeap();
         }
